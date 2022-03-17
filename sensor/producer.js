70542dc8cbc9a5dac9b2aea2305eb633173cee5b
Christian Rengifo Mejia <christian2162133@gmail.com>
2022-03-16 19:50:55 -0500

    sensor commit

--- sensor/producer.js
+++ sensor/producer.js
@@ -1,5 +1,8 @@
 const amqp = require("amqplib");
 const queue = process.env.QUEUE || 'restaurant';
+const type = process.env.TYPE || 'local';
+const name = process.env.NAME || 'local-Random';
+var can = 0;
 const rabbitConfig = {
     protocol: 'amqp',
     hostname: process.env.RABBIT_HOST || 'localhost',
@@ -8,13 +11,31 @@ const rabbitConfig = {
     password: process.env.RABBIT_PASSWORD || '12345'
 }
 
+function cantidad(){
+    var rcan = Math.floor(Math.random()*(5-(1+1))+1);
+    var des = Math.random();
+    if(can == 0){
+        return can = can + rcan;
+    }else if(can > 0){
+        if(des > 0.6){
+            return can = can + rcan;
+        }else{
+            can = can - rcan;
+            if(can <= 0){
+                can = 0;
+            }
+            return can;
+        }
+    }
+}
+
 async function publisher() {
     const connection = await amqp.connect(rabbitConfig)
     const channel = await connection.createChannel()
 
     await channel.assertQueue(queue)
 
-    const message = {"tipo": "test1", "nombre": "nombre prueba 1", "capacidad": "100"}
+    const message = {"tipo": type, "nombre": name, "capacidad": cantidad()}
 
     const sent = await channel.sendToQueue(
         queue,
@@ -33,4 +54,8 @@ async function publisher() {
 publisher()
     .catch(err => {
         console.error(`Error => ${err}`)
-    })
+    });
+
+setInterval(() =>{
+    publisher();
+}, 5000);
